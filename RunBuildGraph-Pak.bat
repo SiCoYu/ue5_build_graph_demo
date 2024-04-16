72ddad6ee3b760d41489b1f9502a0c12c52f2f04
yuxikuo <yuxikuo@h3d.com.cn>
2024-04-16 21:44:20 +0800

    更新BuildGraph功能

--- RunBuildGraph-Pak.bat
+++ RunBuildGraph-Pak.bat
@@ -3,5 +3,5 @@ set CurPath=%~dp0
 set UE5UATPath=D:/WP_YXK/UE/UE_5.3/Engine/Build/BatchFiles/
 rem F:/UE5/UE_Src/UE_5.3_T3/Engine/Build/BatchFiles/
 
-%UE5UATPath%RunUAT.bat BuildGraph -Script=%CurPath%Tools/BuildGraph.xml  -Target="Windows Pak And Stage" -set:UATPath=%UE5UATPath%RunUAT.bat -set:ProjectRoot="E:/WorkPlace/WP_YXK/ue5_build_graph_demo" -set:UProjectPath="E:/WorkPlace/WP_YXK/ue5_build_graph_demo/FirstPersonGame.uproject" -set:StageDirectory="E:\WorkPlace\WP_YXK\ue5_build_graph_demo\PakOutputV2" -NoP4 -WriteToSharedStorage -BuildMachine > Pak.Log
+%UE5UATPath%RunUAT.bat BuildGraph -Script=%CurPath%Tools/BuildGraph.xml  -Target="Windows Pak And Stage" -set:UATPath=%UE5UATPath%RunUAT.bat -set:ProjectRoot="E:/WorkPlace/WP_YXK/ue5_build_graph_demo" -set:UProjectPath="E:/WorkPlace/WP_YXK/ue5_build_graph_demo/FirstPersonGame.uproject" -set:StageDirectory="E:\WorkPlace\WP_YXK\ue5_build_graph_demo\PakOutput" -NoP4 -WriteToSharedStorage -BuildMachine > Pak.Log
 pause
